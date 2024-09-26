0438078ba80ffaae2a7360da62d7f22581a47298
Дарья Конева <Tyuiu.KonevaDD@outlook.com>
2024-09-26 21:44:59 +0500

    Task1

---
+++ Tyuiu.KonevaDD.Sprint1.Task1.V1.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+using Tyuiu.KonevaDD.Sprint1.Task1.V1.Lib;
+namespace Tyuiu.KonevaDD.Sprint1.Task1.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 184.02; double y = 2.0;
+            double a = 4.0; var res = ds.Calculate(a, x, y);
+            Assert.AreEqual(24.5, res);
+
+        }
+    }
+}
