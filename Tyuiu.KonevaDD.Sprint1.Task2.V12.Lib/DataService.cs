2c51318ddb59c33d39352a28035fb698a99df7b1
Дарья Конева <Tyuiu.KonevaDD@outlook.com>
2024-09-26 23:39:38 +0500

    Task2

---
+++ Tyuiu.KonevaDD.Sprint1.Task2.V12.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.KonevaDD.Sprint1.Task2.V12.Lib
+{
+    public class DataService : ISprint1Task2V12
+    {
+      
+        public int CalculateParallelepipedVolume(int value, int valueTwo, int valueThree)
+        {
+            return value * valueTwo * valueThree;
+        }
+    }
+}
---
+++ Tyuiu.KonevaDD.Sprint1.Task2.V12/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Tyuiu.KonevaDD.Sprint1.Task2.V12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            // Ввод данных от пользователя
+            int x, y, z;
+            Console.WriteLine("Длина параллелепипеда X:");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ширина параллелепипеда Y:");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Высота параллелепипеда Z:");
+            z = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+          
+            Console.WriteLine("Объём параллелепипеда = " + CalculateParallelepipedVolume(x, y, z));
+            Console.ReadLine();
+        }
+
+       
+        static int CalculateParallelepipedVolume(int x, int y, int z)
+        {
+            return x * y * z;
+        }
+    }
+}
